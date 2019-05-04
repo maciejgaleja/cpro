8dd7de09524f1deae27f131f08c10f08962cb4cb
Maciej Galeja <maciej.galeja@outlook.com>
2019-05-04 15:35:16 +0200

    Add basic file structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='funniest',
+      version='0.0.0',
+      description='Project structure manager for C/C++',
+      url='',
+      author='Maciej Galeja',
+      author_email='maciej.galeja@outlook.com',
+      license='MIT',
+      packages=['cpro'],
+      zip_safe=False)
